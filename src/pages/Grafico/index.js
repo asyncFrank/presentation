cc88c6f3f35e45dece5126b067d200638c8694fe
asyncFrank <frank.maldonado.developer@gmail.com>
2023-01-10 13:34:42 -0300

    presentation 2.3333

--- src/pages/Grafico/index.js
+++ src/pages/Grafico/index.js
@@ -6,7 +6,7 @@ import {
   LinearScale,
   PointElement,
 } from "chart.js";
-import '../../styles.css'
+
 ChartJs.register(LineElement, CategoryScale, LinearScale, PointElement);
 
 export default function Grafico() {
@@ -67,22 +67,59 @@ export default function Grafico() {
 
   return (
     <div style={{ width: "980px", height: "980px", margin: "4rem 0 0 4rem" }}>
-      <h1 style={{display:"flex",alignContent:"center",alignItems:"center",
-      fontFamily:"Source Sans Pro, sans-serif", fontWeight:"bold", fontSize:"1.3rem",margin:"1.5rem 0 1.5rem 3rem"}}> <span> Matriz de Avaliação de Valor</span></h1>
-      <Line data={data} options={options} style={{marginBottom:"2rem"}}></Line>
-      <h2>
+      <h1
+        style={{
+          display: "flex",
+          alignContent: "center",
+          alignItems: "center",
+          fontFamily: "Source Sans Pro, sans-serif",
+          fontWeight: "bold",
+          fontSize: "1.3rem",
+          margin: "1.5rem 0 1.5rem 3rem",
+        }}
+      >
+        {" "}
+        <span> Matriz de Avaliação de Valor</span>
+      </h1>
+      <Line
+        data={data}
+        options={options}
+        style={{ marginBottom: "2rem" }}
+      ></Line>
+      <h2
+        style={{
+          fontFamily: "Source Sans Pro, sans-serif",
+          marginBottom: "8px",
+        }}
+      >
         <span style={{ background: "#1C74E9", marginRight: "8px" }}>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </span>
         TCP
       </h2>
-      <h2 style={{fontFamily:"Source Sans Pro, sans-serif"}}>
-        <span style={{ background: "#0FAE3A", marginRight: "8px" }}>
+      <h2
+        style={{
+          fontFamily: "Source Sans Pro, sans-serif",
+          marginBottom: "8px",
+        }}
+      >
+        <span
+          style={{
+            background: "#0FAE3A",
+            marginRight: "8px",
+            marginBottom: "8px",
+          }}
+        >
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </span>
         Fonte Mineral
       </h2>
-      <h2 style={{fontFamily:"Source Sans Pro, sans-serif"}}>
+      <h2
+        style={{
+          fontFamily: "Source Sans Pro, sans-serif",
+          marginBottom: "8px",
+        }}
+      >
         <span style={{ background: "#f26c6d", marginRight: "8px" }}>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </span>
